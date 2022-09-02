3c7c665deae492214cdadfe394c93013ee0f79cf
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2022-09-02 12:43:24 +0900

    my first rust coding!!

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+use num::integer;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
+
+fn main() {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
+    });
+
+    println!("Serving on http://localhost:3000...");
+    server
+        .bind("127.0.0.1:3000")
+        .expect("error binding server to address")
+        .run()
+        .expect("error running server");
+}
+
+fn get_index() -> HttpResponse {
+    HttpResponse::Ok().content_type("text/html").body(
+        r#"
+<title>GCD Calculator</title>
+<form action="/gcd" method="post">
+<input type="text" name="n"/>
+<input type="text" name="m"/>
+<button type="submit">Compute GCD</button>
+</form>
+"#,
+    )
+}
+
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Conputing the GCD with zero is boring.");
+    }
+
+    let response = format!(
+        "The greatest common divisor of the numbers {} and {} \
+is <b>{}</b>\n",
+        form.n,
+        form.m,
+        integer::gcd(form.n, form.m)
+    );
+
+    HttpResponse::Ok().content_type("text/html").body(response)
+}
